fe1f7b6c432df9a7a08aa90bb1db634c0f5bff35
Enrique Incio <enrique.incio@gmail.com>
2017-08-26 17:31:40 -0500

    Implemtando Estructuras

---
+++ BusTicket/BusTicket/Models/Rutas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusTicket.Models
+{
+    public struct Rutas
+    {
+        public string Chofer;
+        public string Compania { get; set; }
+        public List<Localizacion> Estadciones { get; set; }
+        public Guid Id { get; set; };
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+    }
+}
